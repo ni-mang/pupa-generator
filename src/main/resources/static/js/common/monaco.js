f2c31c78d423c8e65d32f4592bd6e92bf32a4bc7
362682205@qq.com <362682205@qq.com>
2023-10-11 17:47:12 +0800

    monaco编辑器窗口未加载提示语句

--- src/main/resources/static/js/common/monaco.js
+++ src/main/resources/static/js/common/monaco.js
@@ -2,6 +2,7 @@
 var editor;
 
 function renderMonaco(elemId, value, lang, readOnly){
+    load(elemId);
     require.config({ paths: { 'vs': '/js/monaco/vs' }});
     require.config({'vs/nls': {availableLanguages: {'*':'zh-cn'}}});
     require(['vs/editor/editor.main'],function(){
@@ -14,7 +15,13 @@ function renderMonaco(elemId, value, lang, readOnly){
         });
     });
 }
-
+function load(elemId){
+    var p=document.createElement('p');
+    p.id = "prompt"
+    p.style.color = "red"
+    p.innerHTML='由于下载Monaco编辑器控件需要耗费一定时间，首次打开代码编辑或查看页面时，可能无法正常展开编辑器窗口，请稍作等待，或重复打开当前窗口！';
+    document.getElementById(elemId).after(p)
+}
 
 //写入值
 //var con = 'package com.abc.pupa.business.controller;';
